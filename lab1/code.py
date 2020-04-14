1aaa607b76f5cff20403c93d7243ba3bd2120dcc
Ilia Vinokurov <50335560+iliyavs@users.noreply.github.com>
2020-04-14 15:28:09 +0300

    Code style and exceptions

--- lab1/code.py
+++ lab1/code.py
@@ -1,12 +1,21 @@
 import csv
 
+TEL_NUM = "915783624"
+TARIFF_PARAMETERS = {
+    'koutcall':2,
+    'kincall':0,
+    'ksms':1,
+    'freesms':10
+    }
+
 def csvread():
+    outcall = incall = [0, 0, 0, 0, 0]
     file = open('data.csv')
     reader = csv.reader(file)
     for row in reader:
-        if row[1] == '915783624':
+        if row[1] == TEL_NUM:
             outcall = row
-        if row[2] == '915783624':
+        if row[2] == TEL_NUM:
             incall = row
     return outcall, incall
 
@@ -14,16 +23,15 @@ def tariff(outcall, incall, koutcall, kincall, k):
     minuteout = float(outcall[3])
     minutein = float(incall[3])
     sms = int(outcall[4])
-    sms -= 10
-    if sms < 0: sms = 0
-    bill = minuteout*koutcall + minutein*kincall + sms*k
+    sms = max(0, sms - TARIFF_PARAMETERS["freesms"])
+    bill = minuteout * koutcall + minutein * kincall + sms * k
     return bill
-    
+
 outcall, incall = csvread()
-koutcall = 2               # Выставим
-kincall = 0                # параметры 
-ksms = 1                   # тарифа
-bill = tariff(outcall, incall, koutcall, kincall, ksms)
+if outcall != [0, 0, 0, 0, 0] or incall != [0, 0, 0, 0, 0]:
+    bill = tariff(outcall, incall, TARIFF_PARAMETERS["koutcall"], TARIFF_PARAMETERS["kincall"], TARIFF_PARAMETERS["ksms"])
+else:
+    bill = 0
 f = open('bill.txt', 'w')
 f.write(str(bill))
 f.close()
